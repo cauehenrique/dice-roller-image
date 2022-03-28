849c107fda138a20379fecca391fed2a3a266e2a
cauehenrique <caue313@gmail.com>
2022-03-28 09:59:56 -0300

    replace: TextView -> ImageView

--- app/src/main/java/com/example/dicerollertext/MainActivity.kt
+++ app/src/main/java/com/example/dicerollertext/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -14,13 +14,23 @@ class MainActivity : AppCompatActivity() {
     setContentView(R.layout.activity_main)
 
     val rollButton = findViewById<Button>(R.id.button)
-    val resultTextView = findViewById<TextView>(R.id.resultText)
+    val diceImage = findViewById<ImageView>(R.id.imageView)
 
     rollButton.setOnClickListener {
       val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
       toast.show()
 
-      resultTextView.text = generateRandomNumber(6).toString()
+      val diceImages = arrayOf<Int>(
+        R.drawable.dice_1,
+        R.drawable.dice_2,
+        R.drawable.dice_3,
+        R.drawable.dice_4,
+        R.drawable.dice_5,
+        R.drawable.dice_6
+      )
+
+      val newNumber = generateRandomNumber(6)
+      diceImage.setImageResource(diceImages[newNumber - 1])
     }
   }
 
